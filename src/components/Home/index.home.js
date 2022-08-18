45608d6501a2920db93c47c866e1df0328f8cccf
NichoAdhyatma <nicholausadhyatna@gmail.com>
2022-08-19 00:09:51 +0700

    fix 2

--- src/components/Home/index.home.js
+++ src/components/Home/index.home.js
@@ -3,19 +3,20 @@ import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 const MySwal = withReactContent(Swal);
 
-MySwal.fire({
-  title: "Tugas React Course",
-  timer: 5000,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading();
-  },
-  html: "Album API dan Posts API",
-  icon: "info",
-});
+
 
 const Home = ({ title, description }) => {
   return (
+    MySwal.fire({
+      title: "Tugas React Course",
+      timer: 5000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+      html: "Album API dan Posts API",
+      icon: "info",
+    }),
     <Container>
       <div className="my-5">
         <h1 className="text-center">{title}</h1>
