f4840cafaf9cc054f1d779f4f5da747fe1d72aa2
NichoAdhyatma <nicholausadhyatna@gmail.com>
2022-08-19 00:06:22 +0700

    fix

--- src/components/albums/index.album.js
+++ src/components/albums/index.album.js
@@ -3,15 +3,11 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Collections from "./collections.album";
 
-
 const Albums = ({ title, description }) => {
-  // const { title, description } = props;
-
-
   return (
     <React.Fragment>
       <Container className="mt-5">
-        <h1 class="text-center">{ title }</h1>
+        <h1 className="text-center">{ title }</h1>
         <p className="text-primary text-center">{ description }</p>
         <Collections />
       </Container>
